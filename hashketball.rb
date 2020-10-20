895c5bfc2855c0691ad191c4110d7ace51bf5076
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-10-20 17:29:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -199,14 +199,10 @@ def big_shoe_rebounds
   rebounds = 0 
   game_hash.map do |key, value|
     value[:players].each do |key_2|
-      key_2.each do |key_3, value_3|
-        if key_2[:shoe] > shoe_size 
-          shoe_size = key_2[:shoe]
-        end
-        if key_2[:shoe] == shoe_size
-          rebounds = key_2[:rebounds]
-        end 
-      end
+      if key_2[:shoe] > shoe_size 
+        shoe_size = key_2[:shoe]
+        rebounds = key_2[:rebounds]
+      end 
     end
   end
   rebounds
